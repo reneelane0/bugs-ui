be0110347fec07c3d306270d6fa353aec62088e9
reneelane0 <w0494485@campus.nscc.ca>
2024-10-29 00:10:40 -0300

    got most of the images to work properly

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -29,38 +29,47 @@ export default function Home() {
         return () => {
             ignore = true;
         }
-    }, []);
+    }, [apiUrl]);
 
     return (
         <>
-          <h1>My Bugs</h1>
-          <p>
-            <Link to="/create" className="btn btn-outline-secondary">Add a Bug</Link>
-          </p>
-          {error && <p className="text-danger">{error}</p>} {/* Display error message */}
-          {
-            bugs.length > 0 ?         
-            bugs.map((bug, index) => (
-              <div key={index} className="card mb-3">
-                <div className="card-body">
-                  <div className="d-flex align-items-center position-relative">
-<img src={`${apiUrl}/images/${bug.filename}`} className="thumbnail" alt={bug.name} />
-    
-                    <div className="bug-info">
-                      <h5 className="card-title">{ bug.name }</h5>
-                      <p className="card-text">{ bug.description }<br />{ bug.dateFound }</p>
+            <h1>My Bugs</h1>
+            <p>
+                <Link to="/create" className="btn btn-outline-secondary">Add a Bug</Link>
+            </p>
+            {error && <p className="text-danger">{error}</p>} {/* Display error message */}
+            {
+                bugs.length > 0 ?         
+                bugs.map((bug, index) => (
+                    <div key={index} className="card mb-3">
+                        <div className="card-body">
+                            <div className="d-flex align-items-center position-relative">
+                                <img 
+                                    src={`${apiUrl}/images/${bug.image}`} // Adjusted image URL here
+                                    className="thumbnail" 
+                                    alt={bug.name} 
+                                />
+                                <div className="bug-info">
+                                    <h5 className="card-title">{bug.name}</h5>
+                                    <p className="card-text">
+                                        {bug.description}<br />
+                                        {bug.dateFound}
+                                    </p>
+                                </div>
+                                <div className="position-absolute top-0 end-0">
+                                    <Link to={`/update/${bug.id}`} className="btn btn-light btn-sm">
+                                        <i className="bi bi-pencil-square"></i>
+                                    </Link>&nbsp;              
+                                    <Link to={`/delete/${bug.id}`} className="btn btn-light btn-sm">
+                                        <i className="bi bi-trash"></i>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>            
                     </div>
-    
-                    <div className="position-absolute top-0 end-0">
-                      <Link to={`/update/${bug.id}`} className="btn btn-light btn-sm"><i className="bi bi-pencil-square"></i></Link>&nbsp;              
-                      <Link to={`/delete/${bug.id}`} className="btn btn-light btn-sm"><i className="bi bi-trash"></i></Link>
-                    </div>
-                  </div>
-                </div>            
-              </div>
-            )) :
-            <p>No bugs.</p>
-          }
+                )) :
+                <p>No bugs.</p>
+            }
         </>
-      )
+    );
 }
